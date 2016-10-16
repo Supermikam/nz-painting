a57d8140d48ae43f406973d22d56a92d7bfa2893
Hannah Liu <liuruohannah@gmail.com>
2016-10-16 07:45:25 +0000

    finish home page without inplement shopping cart

--- home.js
+++ home.js
@@ -1,14 +1,58 @@
-/* global $ */
+/* global $ top */
 
-function load(){
+function initializePage(){
+    loadPopular();
+    loadOnSale();
+    pictureInfo();
+    
+}
+
+function loadImageForSection(name,v,section){
+    var index= name+1;
+    var targetId= '#'+section+index;
+    
+    var container = $(targetId);
+    var img = $('<img />',{src: v.src, id: v.id}).appendTo(container);
+    container.find('.name').text(v.name);
+    container.find('.price').text(v.price);
+}
+
+function loadPopular(){
     $.get('Json/popular.json',function(data, success){
         $.each(data, function(index,value){
             $.each(value, function(name,v){
-                var img = $('<img />',{src: v.src}).appendTo($('#pop'));
+              loadImageForSection(name,v,'pop')  
             })
         })
     })
 }
 
-$(document).ready(load);
+function loadOnSale(){
+    $.get('Json/special.json',function(data, success){
+        $.each(data, function(index,value){
+            $.each(value, function(name,v){
+              loadImageForSection(name,v,'onsale')  
+            })
+        })
+    })
+    
+}
+
+function pictureInfo(){
+    
+    $('.image-container').mouseenter(function(){
+        var pHeight = $(this).height();
+        
+        var topPoint = (pHeight - 80).toString() + 'px';
+        
+        $(this).find('.flow-under').animate({top:topPoint}, 250);
+    });
+    
+    $('.image-container').mouseleave(function(){
+        $(this).find('.flow-under').animate({top: '100%'},250);
+    });
+    
+}
+
+$(document).ready(initializePage);
               
